d28b01c122bd68494eb40445ce2dba0f224cc44b
Guilherme Luiz Borges de Oliveira <gui12051@gmail.com>
2022-08-16 15:48:33 -0300

    Update

--- main.dart
+++ main.dart
@@ -1,26 +1,21 @@
 void main(args){
-  int numero;
+  List<int> numeros = <int>[];
 
-  try {
-    numero = int.parse(args[0]);
-  } catch(e) {
-    print('O argumento não é um número inteiro positivo');
-    return;
+  for (var element in args) {
+    element = int.tryParse(element);
+    if(element != null && element > 0){
+      numeros.add(element);
+    }
   }
-  if(numero < 1){
-    print('O argumento não é um número inteiro positivo');
-    return;
-  }
-
-  print(somatorio(numero));
+  numeros.forEach(((element) => somatorio(element)));
 }
 
-int somatorio(int numero){
+void somatorio(int numero){
   int total = 0;
   for(int i=3;i<numero;i++){
     if(i%3==0 || i%5==0){
       total = total + i;
     }
   }
-  return total;
+  print(total);
 }
